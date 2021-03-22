f65f4cdbce1b662b0fb14bcdfce8e273bd9d56d9
Deerjump <billman377@gmail.com>
2021-03-21 23:16:17 -0600

    init

---
+++ src/main/java/io/github/deerjump/menuapi/Menu.java
@@ -0,0 +1,59 @@
+package io.github.deerjump.menuapi;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class Menu implements InventoryHolder {
+
+    protected Inventory inventory;
+
+    public Menu() {
+        this("Menu");
+    }
+
+    public Menu(String title) {
+        this.inventory = Bukkit.createInventory(this, 9, title);
+    }
+
+    public Menu(InventoryType type) {
+        this(type, "Menu");
+    }
+
+    public Menu(InventoryType type, String title) {
+        this.inventory = Bukkit.createInventory(this, type, title);
+    }
+
+    public Menu(int size) {
+        this(size, "Menu");
+    }
+
+    public Menu(int size, String title) {
+        this.inventory = Bukkit.createInventory(this, size, title);
+    }
+
+    public boolean onClick(InventoryClickEvent event) {
+        return true;
+    }
+
+    public abstract void onOpen(Player player);
+
+    public abstract void onClose(Player player);
+
+    protected void fill(ItemStack item) {
+        for(int i = 0; i < inventory.getSize(); i++) {
+            if (inventory.getItem(i) != null ) continue;
+            inventory.setItem(i, item);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("NullableProblems")
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+}
---
+++ src/main/java/io/github/deerjump/menuapi/MenuListener.java
@@ -0,0 +1,29 @@
+package io.github.deerjump.menuapi;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.InventoryHolder;
+
+public class MenuListener implements Listener {
+
+    @EventHandler(ignoreCancelled = true)
+    private void onClick(InventoryClickEvent event) {
+        final InventoryHolder holder = event.getInventory().getHolder();
+        if (!(holder instanceof Menu)) return;
+        event.setCancelled(((Menu) holder).onClick(event));
+    }
+
+    @EventHandler private void onOpen(InventoryOpenEvent event) {
+        final InventoryHolder holder = event.getInventory().getHolder();
+        if (holder instanceof Menu) ((Menu) holder).onOpen((Player)event.getPlayer());
+    }
+
+    @EventHandler private void onClose(InventoryCloseEvent event) {
+        final InventoryHolder holder = event.getInventory().getHolder();
+        if (holder instanceof Menu) ((Menu) holder).onClose((Player)event.getPlayer());
+    }
+}
